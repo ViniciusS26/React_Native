04812203126f7e76e82fb013e3bb0d808166eac3
Vinicius Nunes <viniciusdasilvanunes773@gmail.com>
2026-05-12 09:06:35 -0300

    fex: modificando inputs

--- myapp/src/components/input.tsx
+++ myapp/src/components/input.tsx
@@ -1,7 +1,13 @@
-import { TextInput,StyleSheet } from "react-native"
+import { TextInput,StyleSheet, View} from "react-native"
 
 export function Input(){
-    return <TextInput style={styleInput.stlInput}/>
+    return(
+        <View>
+            <TextInput placeholder="Email" style={styleInput.stlInput}/>
+            <TextInput placeholder="Senha" style={styleInput.stlInput}/>
+
+        </View>
+    )
 }
 
 Input.displayName ="Input"
@@ -11,8 +17,11 @@ const styleInput = StyleSheet.create({
         borderWidth: 1,
         borderColor: '#92b7ff',
         padding: 20,
-        borderRadius: 8,
-        
+        borderRadius: 20,
+        fontFamily: 'Roboto',
+        fontSize: 16,
+        marginBottom: 16,
+     
     }
    
 })
