b1184effbb1ae5a51b6a9d11d59190a3cb8faa9c
Vinicius Nunes <viniciusdasilvanunes773@gmail.com>
2026-05-12 11:20:47 -0300

    fix:criando tela de login

--- myapp/src/app/index.tsx
+++ myapp/src/app/index.tsx
@@ -1,33 +1,48 @@
-import { View, Text, StyleSheet, Image} from 'react-native'
+import { View, Text, StyleSheet, Image, ScrollView} from 'react-native'
 import  { Input } from '@/components/input'
+import { Buttons } from '@/components/buttons'
 
 
 export default function IndexPage(){
     return(
-        <View style={styles.container}>
-           <Image style={styles.ilustrations} source={require("@/assets/logo.png")} />
-            <Text style={styles.title}>Entrar</Text>
-            <Text style={styles.description}>
-                Acesse sua conta com Email e Senha
-            </Text>
-            <Input/>
-        </View>
+         <ScrollView style={{ flex: 1,}} contentContainerStyle={styles.scrollContent}>
+            <View style={styles.container}>
+               
+                    <Image style={styles.ilustrations} source={require("@/assets/logo.png")} />
+                    <Text style={styles.title}>Entrar</Text>
+                    <Text style={styles.description}>
+                        Acesse sua conta com e-mail e senha
+                    </Text>
+
+                    <View style={styles.form}>
+                        <Input placeholder="E-mail" keyboardType='email-address'/>
+                        <Input placeholder="Senha" secureTextEntry/>
+                        <Buttons label="Entrar"/>
+                    </View>
+
+                    <Text style={styles.register}>
+                        Não tem uma conta? <Text style={styles.textTow}>Cadastre-se</Text>
+                    </Text>
+               
+            </View>
+         </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
+    scrollContent:{
+        flexGrow: 1,     
+    },
     container: {
-        flex: 1,
-        backgroundColor: 'rgba(185, 192, 255, 0.67)',
         padding: 32,
     },
 
     ilustrations:{
         width: "100%",
-        height: 250,
-        marginBottom: 32,
+        height: 300,
         alignItems: 'center',
-        marginTop: 70,
+        marginTop: 40,
+        resizeMode: 'contain',
     },
 
     title:{
@@ -40,7 +55,22 @@ const styles = StyleSheet.create({
         fontSize: 18,
         color: '#155882d6',
         fontWeight: 400,
-        marginBottom: 32,
+        marginBottom: 20,
       
+    },
+
+    form:{
+        marginTop: 10,
+        gap:5,
+    },
+    register:{
+        marginTop:20,
+        color: '#8d8d8df4',
+        fontWeight: 600,
+    },
+
+    textTow:{
+        color: '#0043c9f4',
+        fontWeight: 600,
     }
 });
