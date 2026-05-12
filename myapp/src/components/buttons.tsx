b60689f5fa4fa0d5870d6b2fe8f2f2844a41345d
Vinicius Nunes <viniciusdasilvanunes773@gmail.com>
2026-05-12 11:21:31 -0300

    feat: criando componente de buttos

--- myapp/src/components/buttons.tsx
+++ myapp/src/components/buttons.tsx
@@ -1,1 +1,30 @@
-import { TextInput,StyleSheet, View} from "react-native"
+import { StyleSheet,Text, TouchableOpacity, TouchableOpacityProps} from "react-native"
+
+type ButtonProps = TouchableOpacityProps & {
+    label: string;
+}
+
+export function Buttons({ label, ...rest }: ButtonProps){
+    return(
+        <TouchableOpacity style={style.container} {...rest} activeOpacity={0.7}>
+            <Text style={style.lbButton}>{label}</Text>
+        </TouchableOpacity>
+    )
+}
+
+
+const style = StyleSheet.create({
+   container:{
+        width: "100%",
+        height: 48,
+        backgroundColor: '#0043c9f4',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 10,
+   },
+   lbButton:{
+        color: '#ffffff',
+        fontWeight: 600,
+        fontSize: 16,
+   }
+})
